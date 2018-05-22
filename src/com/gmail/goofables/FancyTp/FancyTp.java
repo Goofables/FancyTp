08eaa29616a1b6040bd843b596c1f0cfb0137611
Goofables <goofables@gmail.com>
2018-05-21 20:00:58 -0600

    Fixed crashes. Made my compiler 90% happier

--- src/com/gmail/goofables/FancyTp/FancyTp.java
+++ src/com/gmail/goofables/FancyTp/FancyTp.java
@@ -21,152 +21,164 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class FancyTp extends JavaPlugin implements Listener {
-   ArrayList<Player> frozen = new ArrayList<>();
-   private FileConfiguration config = getConfig();
-   private List<String> disabled;
-   private ConfigurationSection messages;
-   private String cost;
-   private String wait;
-   
-   @Override
-   public void onEnable() {
-      config.addDefault("exclude", new String[] {"MyPlayerName123"});
-      disabled = config.getStringList("exclude");
-      if (!config.isConfigurationSection("messages")) config.createSection("messages");
-      config.addDefault("messages.title", "&aWarping");
-      config.addDefault("messages.subtitle", "&cCost: &6%cost%");
-      config.addDefault("xpCost", ".5%dist%");
-      config.addDefault("xpCost.p", ".5%dist%");
-      config.addDefault("xpCost.r", "1");
-      config.addDefault("tpWait", "45");
-      messages = config.getConfigurationSection("messages");
-      cost = config.getString("xpCost").replaceAll(";", " ");
-      wait = config.getString("tpWait").replaceAll(";", " ");
-      config.options().copyDefaults(true);
-      saveConfig();
-      getServer().getPluginManager().registerEvents(this, this);
-   }
-   
-   @EventHandler
-   public void onPlayerTeleport(PlayerTeleportEvent e) {
-      //System.out.println("Player: " + e.getPlayer() + " From: " + e.getFrom() + " To: " + e.getTo() + " Cause: " + e.getCause());
-      //for (RegisteredListener rl : e.getHandlers().getRegisteredListeners())
-      //   System.out.println(" -> Pl:" + rl.getPlugin().getName() + " RL: " + rl.getListener());
-      final Player player = e.getPlayer();
-      final Location from = e.getFrom().clone();
-      final Location to = e.getTo().clone();
-      if (disabled.contains(player.getName()) || player.hasPermission("fancytp.noeffect")) return;
-      if (frozen.contains(player)) return;
-      // Player is hidden in supper vanish
-      if (Bukkit.getPluginManager().isPluginEnabled("SuperVanish") || Bukkit.getPluginManager().isPluginEnabled("PremiumVanish"))
-         if (VanishAPI.isInvisible(player)) return;
-      
-      // Player is hidden
-      if (Bukkit.getPluginManager().isPluginEnabled("Essentials")) {
-         Essentials ess = (Essentials)Bukkit.getPluginManager().getPlugin("Essentials");
-         User u = ess.getUser(player);
-         if (u != null && u.isHidden()) return;
-      }
-      
-      // If it isnt a command ignore it
-      if (!e.getCause().equals(PlayerTeleportEvent.TeleportCause.COMMAND)) {return;}
-      
-      
-      double distance = from.distance(to);
-      
-      // Get the equation solver
-      ScriptEngineManager mgr = new ScriptEngineManager();
-      ScriptEngine engine = mgr.getEngineByName("javascript");
-      
-      // Calculate cost and wait
-      int expCost = 0;
-      int tpWait = 0;
-      try {
-         Object tmp1 = engine.eval("cost = (" + cost.replace("%dist%", "(" + String.valueOf(distance) + ")") + ");");
-         Object tmp2 = engine.eval("wait = (" + wait.replace("%dist%", "(" + String.valueOf(distance) + ")") + ");");
-         player.sendMessage("cost:" + tmp1 + " wait: " + wait + " dist: " + distance);
-      } catch (ScriptException e1) {
-         System.out.println("Error in tp: ");
-         e1.printStackTrace();
-         return;
-      }
-      
-      if (tpWait < 0) tpWait = 0;
-      if (expCost < 0) expCost = 0;
-      
-      // Cancel the event
-      e.setCancelled(true);
-      
-      // Exp cost management
-      int exp = player.getTotalExperience();
-      if ((player.getGameMode().equals(GameMode.SURVIVAL) || player.getGameMode().equals(GameMode.ADVENTURE)) && !player.hasPermission("fancytp.nocost"))
-         if (expCost > exp) {
+    private static final Pattern TXT_MC = Pattern.compile("&", Pattern.LITERAL);
+    private static final Pattern MC_TXT = Pattern.compile("&", Pattern.LITERAL);
+    private final FileConfiguration config = getConfig();
+    //    private String cost;
+    private static final int wait = 45;
+    ArrayList<Player> frozen = new ArrayList<>(0);
+    private List<String> disabled;
+    private ConfigurationSection messages;
+    
+    @Override
+    public void onEnable() {
+        config.addDefault("exclude", new String[] {"MyPlayerName123"});
+        disabled = config.getStringList("exclude");
+        if (!config.isConfigurationSection("messages")) config.createSection("messages");
+        config.addDefault("messages.title", "&aWarping");
+        config.addDefault("messages.subtitle", "&cCost: &6%cost%");
+        /*config.addDefault("xpCost", ".5%dist%");
+        config.addDefault("xpCost.p", ".5%dist%");
+        config.addDefault("xpCost.r", "1");*/
+        config.addDefault("tpWait", "45");
+        messages = config.getConfigurationSection("messages");
+        //        cost = config.getString("xpCost").replaceAll(";", " ");
+        //wait = config.getString("tpWait").replaceAll(";", " ");
+        config.options().copyDefaults(true);
+        saveConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent e) {
+        //System.out.println("Player: " + e.getPlayer() + " From: " + e.getFrom() + " To: " + e.getTo() + " Cause: " + e.getCause());
+        //for (RegisteredListener rl : e.getHandlers().getRegisteredListeners())
+        //   System.out.println(" -> Pl:" + rl.getPlugin().getName() + " RL: " + rl.getListener());
+        final Player player = e.getPlayer();
+        final Location from = e.getFrom().clone();
+        final Location to = e.getTo().clone();
+        
+        if (disabled.contains(player.getName()) || player.hasPermission("fancytp.noeffect")) return;
+        if (frozen.contains(player)) return;
+        
+        // Player is hidden in supper vanish
+        if (Bukkit.getPluginManager().isPluginEnabled("SuperVanish") ||
+                Bukkit.getPluginManager().isPluginEnabled("PremiumVanish")) if (VanishAPI.isInvisible(player)) return;
+        
+        // Player is hidden in essentials
+        if (Bukkit.getPluginManager().isPluginEnabled("Essentials")) {
+            Essentials ess = (Essentials)Bukkit.getPluginManager().getPlugin("Essentials");
+            User u = ess.getUser(player);
+            if (u != null && u.isHidden()) return;
+        }
+        
+        // If it isnt a command ignore it
+        if (!(e.getCause() == TeleportCause.COMMAND || e.getCause() == TeleportCause.PLUGIN)) return;
+        
+        double distance = (from.getWorld().getName().equals(to.getWorld().getName()))?from.distance(to):null;
+        
+        /*
+        // Get the equation solver
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("javascript");
+        */
+        
+        // Calculate cost and wait
+        int expCost = 0;
+        int tpWait = wait;
+        /*
+        try {
+            Object tmp1 = engine.eval("cost = (" + cost.replace("%dist%", "(" + String.valueOf(distance) + ")") + ");");
+            Object tmp2 = engine.eval("wait = (" + wait.replace("%dist%", "(" + String.valueOf(distance) + ")") + ");");
+            player.sendMessage("cost:" + tmp1 + " wait: " + wait + " dist: " + distance);
+        } catch (ScriptException e1) {
+            System.out.println("Error in tp: ");
+            e1.printStackTrace();
+            return;
+        }*/
+        
+        if (tpWait < 0) tpWait = 0;
+        if (expCost < 0) expCost = 0;
+        
+        // Cancel the event
+        // Event is canceled here because it is the last place before the teleport task.
+        e.setCancelled(true);
+        
+        // Exp cost management
+        int exp = player.getTotalExperience();
+        if ((player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) &&
+                !player.hasPermission("fancytp.nocost")) if (expCost > exp) {
             player.sendMessage("§cNot enough exp! Required: §6" + expCost + "§c Currently Have: §6" + exp + "§c!");
             return;
-         } else Bukkit.getScheduler().runTask(this, new ExpTask(this, player, expCost, tpWait + 5 /*45 ticks per tp*/));
-      else expCost = -1;
-      
-      player.sendTitle(messages.getString("title").replace("&", "§"), messages.getString("subtitle").replace("&", "§").replace("%cost%", (expCost >= 0)?String.valueOf(expCost):"free"), 10, 30, 5);
-      frozen.add(player);
-      Bukkit.getScheduler().runTask(this, new EffectTask(this, from, to, tpWait));
-      Bukkit.getScheduler().runTaskLater(this, new TpTask(this, player, from, to), tpWait);
-   }
-   
-   @EventHandler
-   public void onPlayerMove(PlayerMoveEvent e) {
-      if (frozen.contains(e.getPlayer()))
-         if (!e.getTo().getBlock().equals(e.getFrom().getBlock())) e.setTo(e.getFrom().clone());
-   }
-   
-   @Override
-   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-      switch (command.getName().toLowerCase()) {
-         case "fancytp":
-            if (args.length < 1) return false;
-            if (args[0].equals("config")) {}
-            Player target = (Player)sender;
-            if (args.length > 1) target = getServer().getPlayer(args[1]);
-            if (target == null) {
-               sender.sendMessage("§cError! Could not find player `§o" + args[1] + "§c`.");
-               return true;
-            }
-            String targetName = target.getName();
-            switch (args[0].toLowerCase()) {
-               case "toggle":
-                  if (disabled.contains(targetName)) disabled.remove(targetName);
-                  else disabled.add(targetName);
-                  break;
-               case "off":
-                  if (!disabled.contains(targetName)) disabled.add(targetName);
-                  break;
-               case "on":
-                  if (disabled.contains(targetName)) disabled.remove(targetName);
-                  break;
-               default:
-                  return false;
-            }
-            config.set("exclude", disabled);
-            saveConfig();
-            sender.sendMessage("§6FancyTp " + (disabled.contains(targetName)?"§cdisabled":"§aenabled") + " §6for player" + target.getDisplayName() + ".");
-            return true;
-         case "tp":
-            StringBuilder com = new StringBuilder("minecraft:tp");
-            for (String s : args)
-               com.append(" ").append(s);
-            getServer().dispatchCommand(sender, com.toString());
-            return true;
-      }
-      return false;
-   }
-   
+        } else Bukkit.getScheduler().runTask(this, new ExpTask(this, player, expCost, tpWait + 5 /*45 ticks per tp*/));
+        else expCost = -1;
+        
+        player.sendTitle(TXT_MC.matcher(messages.getString("title")).replaceAll(Matcher.quoteReplacement("§")),
+                MC_TXT.matcher(messages.getString("subtitle")).replaceAll(Matcher.quoteReplacement("§")).replace(
+                        "%cost%", (expCost >= 0)?String.valueOf(expCost):"free"), 10, 30, 5);
+        frozen.add(player);
+        Bukkit.getScheduler().runTask(this, new EffectTask(this, from, to, tpWait));
+        Bukkit.getScheduler().runTaskLater(this, new TpTask(this, player, from, to), (long)tpWait);
+    }
+    
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        if (frozen.contains(e.getPlayer()))
+            if (!e.getTo().getBlock().equals(e.getFrom().getBlock())) e.setTo(e.getFrom().clone());
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getName().toLowerCase()) {
+            case "fancytp":
+                if (args.length < 1) return false;
+                if ("config".equals(args[0])) {sender.sendMessage("§cError! Command not setup yet.");}
+                Player target = (Player)sender;
+                if (args.length > 1) target = getServer().getPlayer(args[1]);
+                if (target == null) {
+                    sender.sendMessage("§cError! Could not find player `§o" + args[1] + "§c`.");
+                    return true;
+                }
+                String targetName = target.getName();
+                switch (args[0].toLowerCase()) {
+                    case "toggle":
+                        if (disabled.contains(targetName)) disabled.remove(targetName);
+                        else disabled.add(targetName);
+                        break;
+                    case "off":
+                        if (!disabled.contains(targetName)) disabled.add(targetName);
+                        break;
+                    case "on":
+                        if (disabled.contains(targetName)) disabled.remove(targetName);
+                        break;
+                    default:
+                        return false;
+                }
+                config.set("exclude", disabled);
+                saveConfig();
+                sender.sendMessage(
+                        "§6FancyTp " + (disabled.contains(targetName)?"§cdisabled":"§aenabled") + " §6for player " +
+                                target.getDisplayName() + '.');
+                return true;
+            case "tp":
+                StringBuilder com = new StringBuilder("minecraft:tp");
+                for (String s : args)
+                    com.append(' ').append(s);
+                getServer().dispatchCommand(sender, com.toString());
+                return true;
+        }
+        return false;
+    }
+    
 }
 
--- src/com/gmail/goofables/FancyTp/Tasks.java
+++ src/com/gmail/goofables/FancyTp/Tasks.java
@@ -4,77 +4,82 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 class ExpTask implements Runnable {
-   private Player player = null;
-   private int cost = 0;
-   private int ticks = 0;
-   private FancyTp plugin;
-   
-   ExpTask(FancyTp plugin, Player player, int cost, int ticks) {
-      this.player = player;
-      this.plugin = plugin;
-      this.cost = cost;
-      this.ticks = ticks;
-   }
-   
-   @Override
-   public void run() {
-      if (cost <= 0 || ticks <= 0) return;
-      int xp = player.getTotalExperience();
-      player.setExp(0);
-      player.setLevel(0);
-      player.setTotalExperience(0);
-      int amt = (int)Math.ceil(cost / ticks);
-      player.giveExp(xp - amt);
-      cost -= amt;
-      ticks--;
-      Bukkit.getScheduler().runTaskLater(this.plugin, this, 1); // Repeat?
-   }
+    private final FancyTp plugin;
+    private final Player player;
+    private int cost;
+    private int ticks;
+    
+    ExpTask(FancyTp plugin, Player player, int cost, int ticks) {
+        super();
+        this.player = player;
+        this.plugin = plugin;
+        this.cost = cost;
+        this.ticks = ticks;
+    }
+    
+    @Override
+    public void run() {
+        if (cost <= 0 || ticks <= 0) return;
+        int xp = player.getTotalExperience();
+        player.setExp(0.0f);
+        player.setLevel(0);
+        player.setTotalExperience(0);
+        int amt = (int)Math.ceil((double)(cost / ticks));
+        player.giveExp(xp - amt);
+        cost -= amt;
+        ticks--;
+        Bukkit.getScheduler().runTaskLater(plugin, this, 1); // Repeat?
+    }
 }
 
 class TpTask implements Runnable {
-   private Player player;
-   private FancyTp plugin;
-   private Location from, to;
-   
-   TpTask(FancyTp plugin, Player player, Location from, Location to) {
-      this.player = player;
-      this.plugin = plugin;
-      this.from = from;
-      this.to = to;
-   }
-   
-   @Override
-   public void run() {
-      Bukkit.getScheduler().runTaskLater(plugin, () -> plugin.frozen.remove(player), 5);
-      
-      to.getWorld().spawnParticle(Particle.SMOKE_LARGE, from.clone().add(0, .5, 0), 500, 0, .25, 0, .1);
-      from.getWorld().spawnParticle(Particle.END_ROD, to.clone().add(0, .5, 0), 500, 0, .5, 0, .25);
-      from.getWorld().strikeLightningEffect(from);
-      to.getWorld().strikeLightningEffect(to);
-      player.teleport(to, PlayerTeleportEvent.TeleportCause.PLUGIN);
-   }
+    private final Player player;
+    private final FancyTp plugin;
+    private final Location from;
+    private final Location to;
+    
+    TpTask(FancyTp plugin, Player player, Location from, Location to) {
+        super();
+        this.player = player;
+        this.plugin = plugin;
+        this.from = from;
+        this.to = to;
+    }
+    
+    @Override
+    public void run() {
+        Bukkit.getScheduler().runTaskLater(plugin, () -> plugin.frozen.remove(player), 5);
+        
+        to.getWorld().spawnParticle(Particle.SMOKE_LARGE, from.clone().add(0, .5, 0), 500, 0, .25, 0, .1);
+        from.getWorld().spawnParticle(Particle.END_ROD, to.clone().add(0, .5, 0), 500, 0, .5, 0, .25);
+        from.getWorld().strikeLightningEffect(from);
+        to.getWorld().strikeLightningEffect(to);
+        player.teleport(to, TeleportCause.PLUGIN);
+    }
 }
 
 class EffectTask implements Runnable {
-   private FancyTp plugin;
-   private Location from, to;
-   private int time;
-   
-   EffectTask(FancyTp plugin, Location from, Location to, int time) {
-      this.plugin = plugin;
-      this.from = from;
-      this.to = to;
-      this.time = time;
-   }
-   
-   @Override
-   public void run() {
-      from.getWorld().spawnParticle(Particle.PORTAL, from.clone().add(0, .5, 0), 1000, .1, .5, .1, 1);
-      to.getWorld().spawnParticle(Particle.PORTAL, to.clone().add(0, .5, 0), 1000, .1, .5, .1, 1);
-      time -= 1;
-      if (time > 40) Bukkit.getScheduler().runTaskLater(plugin, this, 1);
-   }
+    private final FancyTp plugin;
+    private final Location from;
+    private final Location to;
+    private int time;
+    
+    EffectTask(FancyTp plugin, Location from, Location to, int time) {
+        super();
+        this.plugin = plugin;
+        this.from = from;
+        this.to = to;
+        this.time = time;
+    }
+    
+    @Override
+    public void run() {
+        from.getWorld().spawnParticle(Particle.PORTAL, from.clone().add(0, .5, 0), 1000, .1, .5, .1, 1);
+        to.getWorld().spawnParticle(Particle.PORTAL, to.clone().add(0, .5, 0), 1000, .1, .5, .1, 1);
+        time -= 1;
+        if (time > 40) Bukkit.getScheduler().runTaskLater(plugin, this, 1L);
+    }
 }
